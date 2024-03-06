2c7b40b800a432d24218f86ccc88304771b1c461
JacoArrpyave <jacobo.arroyave46095@ucaldas.edu.co>
2024-03-05 21:36:04 -0500

    prueba de static y funciones con calses

--- Session3/Main.java
+++ Session3/Main.java
@@ -1,11 +1,20 @@
 public class Main {
-    static Person inst_person = new Person();
+    static Persona persona1 = new Persona();
+    static Persona bryan = new Persona();
+
     static Animal inst_animal = new Animal();
 
     public static void main(String[] args) {
-        inst_person.inputDataUser();
-        inst_animal.showInformation();
-        
+        // persona1.inputDataUser();
+        // inst_animal.showInformation();
+
+       Vehiculo[] vehiculos = new Vehiculo[3]; 
+
+       vehiculos[0] = new Vehiculo(4, "rojo", true);
+       vehiculos[1] = new Vehiculo(2, "azul", false);
+
+       System.out.println(vehiculos[0].cantidadRuedas); // 4
+       System.out.println(Vehiculo.cantidadDeVehiculos); // 2
     }
     
 }
---
+++ Session3/Vehiculo.java
@@ -0,0 +1,14 @@
+public class Vehiculo {
+    public static int cantidadDeVehiculos = 0;
+
+    public int cantidadRuedas;
+    public String color;
+    public boolean esGasolina;
+
+    public Vehiculo(int cantidadRuedas, String color, boolean esGasolina){
+        this.cantidadRuedas = cantidadRuedas;
+        this.color = color;
+        this.esGasolina = esGasolina;
+        Vehiculo.cantidadDeVehiculos++;
+    }
+}
